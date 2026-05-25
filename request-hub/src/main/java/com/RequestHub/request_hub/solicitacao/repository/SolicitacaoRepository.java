453556ee3aa6333366827953f909a6925b30c450
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-05-25 20:20:16 -0300

    Repository pronto!!

---
+++ request-hub/src/main/java/com/RequestHub/request_hub/solicitacao/repository/SolicitacaoRepository.java
@@ -0,0 +1,29 @@
+package com.RequestHub.request_hub.solicitacao.repository;
+
+import com.RequestHub.request_hub.solicitacao.domain.Solicitacao;
+import com.RequestHub.request_hub.solicitacao.domain.StatusSolicitacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+public interface SolicitacaoRepository extends JpaRepository<Solicitacao, UUID> {
+
+  /*
+    Solicitante listar suas solicitações
+   */
+  List<Solicitacao> findBySolicitanteId(UUID solicitanteId);
+
+  /*
+    Admin listar por status
+   */
+  List<Solicitacao> findByStatus(StatusSolicitacao status);
+
+    /*
+    Buscar solicitação específica do solicitante
+       */
+    Optional<Solicitacao> findByIdAndSolicitanteId(UUID id, UUID solicitanteId);
+}
