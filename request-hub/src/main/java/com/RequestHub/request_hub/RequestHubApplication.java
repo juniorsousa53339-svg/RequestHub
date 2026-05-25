b23a9be9253fd65fc609ff18092e8ecbed0b06f6
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-05-25 19:23:16 -0300

    primeiro commit

---
+++ request-hub/src/main/java/com/RequestHub/request_hub/RequestHubApplication.java
@@ -0,0 +1,13 @@
+package com.RequestHub.request_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestHubApplication.class, args);
+	}
+
+}
---
+++ request-hub/src/test/java/com/RequestHub/request_hub/RequestHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RequestHub.request_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
