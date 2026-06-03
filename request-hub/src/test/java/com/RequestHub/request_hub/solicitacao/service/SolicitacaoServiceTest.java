b4aa1970606a4478b71df329c3046c54c4cddf1e
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-06-03 11:04:22 -0300

    Back-end Finalizado!!, proximos passos Criar projeto Angular

--- request-hub/src/test/java/com/RequestHub/request_hub/solicitacao/service/SolicitacaoServiceTest.java
+++ request-hub/src/test/java/com/RequestHub/request_hub/solicitacao/service/SolicitacaoServiceTest.java
@@ -32,15 +32,13 @@ class SolicitacaoServiceTest {
 
     @Test
     void deveLancarNotFound_quandoSolicitacaoNaoExistir() {
-        // Arrange
+
         UUID id = UUID.randomUUID();
         when(solicitacaoRepository.findById(id)).thenReturn(Optional.empty());
 
-        // Act + Assert
         assertThrows(NotFoundException.class,
                 () -> solicitacaoService.alterarStatus(id, StatusSolicitacao.EM_ANDAMENTO));
 
-        // Assert (garante que não tentou persistir)
         verify(solicitacaoRepository, never()).save(any());
     }
 
@@ -52,7 +50,6 @@ class SolicitacaoServiceTest {
         solicitacao.setId(id);
         solicitacao.setStatus(StatusSolicitacao.ABERTA);
 
-        // Pré-condição (te ensina a debugar)
         assertEquals(StatusSolicitacao.ABERTA, solicitacao.getStatus());
 
         when(solicitacaoRepository.findById(id)).thenReturn(Optional.of(solicitacao));
@@ -73,7 +70,6 @@ class SolicitacaoServiceTest {
         solicitacao.setId(id);
         solicitacao.setStatus(StatusSolicitacao.FINALIZADA);
 
-        // Pré-condição
         assertEquals(StatusSolicitacao.FINALIZADA, solicitacao.getStatus());
 
         when(solicitacaoRepository.findById(id)).thenReturn(Optional.of(solicitacao));
@@ -116,4 +112,70 @@ class SolicitacaoServiceTest {
         verify(solicitacaoRepository, times(1)).save(captor.capture());
     }
 
+    @Test
+    void deveLancarNotFound_quandoAlterarSolicitacaoNaoExistir() {
+
+        UUID id = UUID.randomUUID();
+        when(solicitacaoRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class,
+                () -> solicitacaoService.alterarSolicitacao(id,"Antigo","Antiga"));
+
+        verify(solicitacaoRepository, never()).save(any());
+
+        verify(solicitacaoRepository, times(1)).findById(id);
+    }
+
+    @Test
+    void deveDeletarSolicitacao() throws Exception {
+
+        UUID id = UUID.randomUUID();
+
+        Solicitacao solicitacao = new Solicitacao();
+        solicitacao.setId(id);
+        solicitacao.setStatus(StatusSolicitacao.ABERTA);
+
+        when(solicitacaoRepository.findById(id)).thenReturn(Optional.of(solicitacao));
+
+        solicitacaoService.deletarSolicitacao(id);
+
+        ArgumentCaptor<Solicitacao> captor = ArgumentCaptor.forClass(Solicitacao.class);
+        verify(solicitacaoRepository).delete(captor.capture());
+
+        assertEquals(id,captor.getValue().getId());
+
+        verify(solicitacaoRepository, times(1)).delete(captor.capture());
+    }
+
+
+    @Test
+    void deveLancarNotFound_quandoDeletarSolicitacaoNaoExistir() {
+
+        UUID id = UUID.randomUUID();
+        when(solicitacaoRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class,
+                () -> solicitacaoService.deletarSolicitacao(id));
+
+        verify(solicitacaoRepository, times(1)).findById(id);
+    }
+
+    @Test
+    void deveLancarBusinessException_quandoStatusNaoPermitirExcluir() {
+
+        UUID id = UUID.randomUUID();
+
+         Solicitacao solicitacao = new Solicitacao();
+        solicitacao.setId(id);
+        solicitacao.setStatus(StatusSolicitacao.EM_ANDAMENTO);
+
+        assertEquals(StatusSolicitacao.EM_ANDAMENTO, solicitacao.getStatus());
+
+        when(solicitacaoRepository.findById(id)).thenReturn(Optional.of(solicitacao));
+
+        assertThrows(BusinessException.class,
+                () -> solicitacaoService.deletarSolicitacao(id));
+
+        verify(solicitacaoRepository, never()).delete(any());
+    }
 }
