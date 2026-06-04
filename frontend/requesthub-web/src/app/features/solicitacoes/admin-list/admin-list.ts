ba15bf5f1f8010eee3b89fa9b63bcc486a17f7f3
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-06-04 17:58:19 -0300

    Infreestrutura no Angular Feita

---
+++ frontend/requesthub-web/src/app/features/solicitacoes/admin-list/admin-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-list',
+  imports: [],
+  templateUrl: './admin-list.html',
+  styleUrl: './admin-list.scss',
+})
+export class AdminList {}
---
+++ frontend/requesthub-web/src/app/features/solicitacoes/edit/edit.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit',
+  imports: [],
+  templateUrl: './edit.html',
+  styleUrl: './edit.scss',
+})
+export class Edit {}
