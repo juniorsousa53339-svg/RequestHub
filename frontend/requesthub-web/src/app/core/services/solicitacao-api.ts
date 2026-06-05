0b2bef66faaf50e0713666f9b45a1a80c95bc03a
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-06-05 12:21:38 -0300

    refactor(frontend): centraliza baseUrl no environment e tipa listarTodas

--- frontend/requesthub-web/src/app/core/services/solicitacao-api.ts
+++ frontend/requesthub-web/src/app/core/services/solicitacao-api.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { SolicitacaoResponse } from '../models/solicitacao-response';
+import {environment} from '../../../environments/environment';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class SolicitacaoApi {}
+
+export class SolicitacaoApiService {
+
+private readonly baseUrl = `${environment.apiUrl}/solicitacoes`;
+
+  constructor(private http: HttpClient ) { }
+
+ public listarTodas() : Observable<SolicitacaoResponse[]> {
+    return this.http.get<SolicitacaoResponse[]>(this.baseUrl);
+  }
+
+}
+
